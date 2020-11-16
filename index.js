0d5df15020ef80388452592e862e8b58dbfd3ab9
p.p <554096147@qq.com>
2020-11-16 17:56:03 +0800

    fix:移除混淆代码

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/index.js");
-} else {
-  module.exports = require("./build/bundle.min.js");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./dist/index.js");
+// } else {
+//   module.exports = require("./build/bundle.min.js");
+// }
+module.exports = require("./dist/index.js");
